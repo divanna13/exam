045f7507ab50a70096c5bae65aafb07cfa3e7b81
divanna13 <86765372+divanna13@users.noreply.github.com>
2024-11-27 14:47:45 +0700

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import TaskViewSet
+from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet)
@@ -8,12 +11,6 @@ router.register(r'tasks', TaskViewSet)
 urlpatterns = [
     path('', include(router.urls)),
 ]
-
-
-from django.urls import path
-from rest_framework.routers import DefaultRouter
-from .views import TaskViewSet
-from . import views
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet)
@@ -22,10 +19,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('tasks/', views.task_list, name='task-list'),
 ]
-
-
-from django.contrib import admin
-from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
