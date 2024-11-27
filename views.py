61290a94a307eaa971330deecd35274bbd396072
divanna13 <86765372+divanna13@users.noreply.github.com>
2024-11-27 15:00:43 +0700

    Update views.py

--- views.py
+++ views.py
@@ -2,15 +2,12 @@
 from rest_framework import viewsets
 from .models import Task
 from .serializers import TaskSerializer
+from django.shortcuts import render
+from .models import Task
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-
-
-from django.shortcuts import render
-from .models import Task
-
 def task_list(request):
     tasks = Task.objects.all()
     return render(request, 'tasks/task_list.html', {'tasks': tasks})
